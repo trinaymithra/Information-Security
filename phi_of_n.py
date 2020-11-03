47a83e5470dfeae5db8d37ac468de3fd40371f30
TRINAY MITHRA <trinaymithra@gmail.com>
2020-11-03 10:56:27 +0530

    Update phi_of_n.py

--- phi_of_n.py
+++ phi_of_n.py
@@ -1,9 +1,9 @@
-import sympy as sp
+import sympy as sp #for prime factors
 
 def factorization_of_n (n) :
-#returns a dictionary with prime factorization of n
+#returns a dictionary of proper prime factorization of n
 
-  	da = dict()
+	da = dict()
 	prime_list = sp.primefactors(n)
 	prime_list = [int(i) for i in prime_list]
 	for i in prime_list :
@@ -11,14 +11,13 @@ def factorization_of_n (n) :
 		while n % (i**counter) == 0 :
 			counter +=1
 		da[i] = counter-1
-	return da
-  
 
-def phi_of_n ():
+	return da
+
+def phi_of_n (n):
 #totient coefficient or Euiler's phi value calculation
 
-	n = int(input())
-	a=0
+	a=1
 	if n==1 :
 		pass
 	elif sp.isprime(n) == True :
@@ -26,8 +25,8 @@ def phi_of_n ():
 	else :
 		dnum = factorization_of_n(n)
 		for key in dnum.keys() :
-			a=a+key**(dnum[key]-1)
+			a=a * (key**dnum[key] - key**(dnum[key]-1))
 	print ("phi of {} = {}".format(n,a))
 
-
-phi_of_n()
+n = int(input("Enter the number for which phi has to be calclulated: "))
+phi_of_n(n)
